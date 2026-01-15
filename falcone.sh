ff53216328b3fa2e1972f32cfd826266734e2c8a
andrea <andrea@clariteia.com>
2026-01-15 17:12:29 +0000

    working in progress

---
+++ falcone.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# variables
+os=$(uname)
+RED="\e[31m"
+# check if is sudo
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}This script must be run as root${ENDCOLOR}"
+   exit 1
+fi
+
+if [ "$os" = "Linux" ]; then
+  if [[ -f /etc/redhat-release ]]; then
+    os_system="redhat"
+  elif [[ -f /etc/debian_version ]]; then
+    os_system="debian"
+  fi
+  script_folder="./scripts/${os_system}"
+  echo -e "${RED}Wait please...${ENDCOLOR}"
+  bash "${script_folder}/deps.sh" >/dev/null 2>&1
+  bash "${script_folder}/gum.sh" >/dev/null #2>&1
+  gum style \
+	--foreground 212 --border-foreground 212 --border double \
+	--align center --width 50 --margin "1 2" --padding "2 4" \
+	'Falcone CLI (1¢)' 'So sweet and so fresh!'
+else
+  echo -e "${RED}Unsupported OS${ENDCOLOR}"
+  exit 1
+fi
---
+++ scripts/debian/gum.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p /etc/apt/keyrings
+        curl -fsSL https://repo.charm.sh/apt/gpg.key | gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | tee /etc/apt/sources.list.d/charm.list
+        apt update
+        apt install gum -y
